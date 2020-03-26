f46a3b2590382c51c1f4de7a78fbb5cc343faae7
Rodney Maida Reinaga <rodmaidareinaga@gmail.com>
2020-03-26 14:52:25 -0400

    Refactor main - created run, print, showMenu and readOption methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,42 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        Main main = new Main();
+        main.run();
+
+    }
+
+    private void run() {
         // User details
         String name = "Jerry";
         String account = "BO10006789";
         // Welcome to the BankApp
-        System.out.println("Welcome " + name);
-        System.out.println("Your current account is " + account);
+        print("Welcome " + name);
+        print("Your current account is " + account);
         // Main Menu
-        System.out.println(System.lineSeparator());
-        System.out.println("********************");
-        System.out.println("(W) Withdraw");
-        System.out.println("(D) Deposit");
-        System.out.println("(B) My Balance");
-        System.out.println("(T) My Transactions");
-        System.out.println("(E) Exit");
-        System.out.println("********************");
+        showMenu();
         // Press a key to select an option
+        readOption();
+    }
+
+    public void print(String message) {
+        System.out.println(message);
+    }
+
+    private void showMenu() {
+        // Main Menu
+        print(System.lineSeparator());
+        print("********************");
+        print("(W) Withdraw");
+        print("(D) Deposit");
+        print("(B) My Balance");
+        print("(T) My Transactions");
+        print("(E) Exit");
+        print("********************");
+    }
+
+    private void readOption() {
         try (Scanner scanner = new Scanner(System.in)) {
             char option;
             do {
@@ -26,14 +46,14 @@ public class Main {
                 option = (char) scanner.nextLine().trim().toUpperCase().charAt(0);
                 switch (option) {
                     case 'E':
-                        System.out.println("Exit...");
+                        print("Exit...");
                         break;
                     default:
-                        System.out.println(option + " is an invalid option");
+                        print(option + " is an invalid option");
                         break;
                 }
-                System.out.println(System.lineSeparator());
-                System.out.println("(A) Show All options");
+                print(System.lineSeparator());
+                print("(A) Show All options");
             } while (option != 'E');
         }
     }
